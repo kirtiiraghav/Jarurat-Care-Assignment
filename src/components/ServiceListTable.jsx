7fb9479a838d89e02bdacb30a5b99f5cef9d8c41
kirti <kirti.20cs@saividya.ac.in>
2024-09-26 16:59:58 +0530

    fix: resolve layout issue in service list table

--- src/components/ServiceListTable.jsx
+++ src/components/ServiceListTable.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ServiceListTable({ serviceData }) {
 
     return (
-        <table className="w-full border-collapse mt-[17px] self-start">
+        <table className="w-full border-collapse mt-[17px]">
             <thead>
                 <tr>
                     <th className='border-b border-black'>Service Name</th>
@@ -22,9 +22,11 @@ export default function ServiceListTable({ serviceData }) {
                         <td className='border-b border-black p-1'>{data.name}</td>
                         <td className='border-b border-black p-1'>{data.description}</td>
                         <td className='border-b border-black p-1'>₹{data.price}</td>
-                        <td className='border-b border-black flex items-center justify-center gap-2'>
-                            <button className='bg-black text-white p-2 rounded hover:bg-white hover:text-black hover:border-black border my-2'>Edit</button>
-                            <button className=' hover:text-red-600'>Delete</button>
+                        <td className='border-b border-black p-1'>
+                            <div className='flex items-center justify-center gap-2'>
+                                <button className='bg-black text-white p-2 rounded hover:bg-white hover:text-black hover:border-black border my-2'>Edit</button>
+                                <button className=' hover:text-red-600'>Delete</button>
+                            </div>
                         </td>
                     </tr>
                 })}
