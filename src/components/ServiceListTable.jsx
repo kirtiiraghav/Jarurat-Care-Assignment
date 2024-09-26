e3ebbb56a6ff70ed3785a021db2150f0697ebaa5
kirti <kirti.20cs@saividya.ac.in>
2024-09-26 16:15:00 +0530

    add Edit and Delete buttons

--- src/components/ServiceListTable.jsx
+++ src/components/ServiceListTable.jsx
@@ -3,24 +3,29 @@ import React from 'react'
 export default function ServiceListTable({ serviceData }) {
 
     return (
-        <table className="w-full border-collapse mt-[17px] self-start border border-black">
+        <table className="w-full border-collapse mt-[17px] self-start">
             <thead>
                 <tr>
-                    <th className='border border-black'>Service Name</th>
-                    <th className='border border-black'>Description</th>
-                    <th className='border border-black'>
+                    <th className='border-b border-black'>Service Name</th>
+                    <th className='border-b border-black'>Description</th>
+                    <th className='border-b border-black'>
                         <div className="flex">
                             <span className='mr-2'>Price</span>
                         </div>
                     </th>
+                    <th className='border-b border-black'></th>
                 </tr>
             </thead>
             <tbody>
                 {serviceData.map((data) => {
                     return <tr key={data.id}>
-                        <td className='border border-black p-1'>{data.name}</td>
-                        <td className='border border-black p-1'>{data.description}</td>
-                        <td className='border border-black p-1'>₹{data.price}</td>
+                        <td className='border-b border-black p-1'>{data.name}</td>
+                        <td className='border-b border-black p-1'>{data.description}</td>
+                        <td className='border-b border-black p-1'>₹{data.price}</td>
+                        <td className='border-b border-black flex items-center justify-center gap-2'>
+                            <button className='bg-black text-white p-2 rounded hover:bg-white hover:text-black hover:border-black border my-2'>Edit</button>
+                            <button className=' hover:text-red-600'>Delete</button>
+                        </td>
                     </tr>
                 })}
             </tbody>
