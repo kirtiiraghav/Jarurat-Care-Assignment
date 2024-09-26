180359667cf1ef60e01c54318f6bb41949e847f4
kirti <kirti.20cs@saividya.ac.in>
2024-09-26 11:52:04 +0530

    add a button to submit form

--- src/components/ServiceForm.jsx
+++ src/components/ServiceForm.jsx
@@ -4,10 +4,11 @@ import DescriptionInput from './DescriptionInput'
 
 export default function ServiceForm() {
     return (
-        <form>
+        <form className='w-full'>
             <Input label='Service Name' id='name' />
             <DescriptionInput label='Description' id='desc' />
             <Input label='Price' id='price' />
+            <button className='bg-gray-300 w-full rounded p-1 text-center'>Add</button>
         </form>
     )
 }
