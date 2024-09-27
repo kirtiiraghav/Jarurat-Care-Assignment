0ee33e72919d52053a32a95cd821268dfaff7cf4
kirti <kirti.20cs@saividya.ac.in>
2024-09-27 15:41:33 +0530

    style: add bg color and z-index to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/images/logo.svg'
 
 export default function Header() {
     return (
-        <header className='p-4 shadow sticky top-0' >
+        <header className='p-4 shadow sticky top-0 z-10 bg-white' >
             <a href="#">
                 <img className="inline-block w-[8rem] sm:w-[10rem]" src={logo} alt="logo" />
             </a>
